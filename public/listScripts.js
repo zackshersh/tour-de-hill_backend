0f8af6e9a98481224798eea17b64414b1b8548c1
Zack Hersh <zackshersh@gmail.com>
2021-10-01 23:32:18 -0400

    await

--- public/listScripts.js
+++ public/listScripts.js
@@ -6,10 +6,10 @@ async function buildList(){
     console.log(data)
 }
 
-function getCompetitors(){
+async function getCompetitors(){
     fetch('https://tour-de-hill.herokuapp.com/api/competitors')
         .then((res) => {
-            let data = res.json();
+            let data = await res.json();
             console.log(res)
             console.log(data);
             return data;

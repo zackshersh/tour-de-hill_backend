c3d7f9646dffd94f4c72c9917946d7b3aa000a8b
Zack Hersh <zackshersh@gmail.com>
2021-10-01 20:50:44 -0400

    stuff

--- controllers/index.js
+++ controllers/index.js
@@ -10,7 +10,17 @@ router.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
-router.post('/api/signup', async (req, res) => {
+router.get('/api/competitors', async (req,res) => {
+    try {
+        const allComps = await Competitor.findAll();
+
+        res.status(200).json(allComps)
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
+router.post('/api/competitor', async (req, res) => {
     try {
         const compData = await Competitor.create({
             first_name: req.body.first_name,

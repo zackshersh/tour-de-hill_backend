64232b5bb529c3c98f037023476e3c34acfcf21a
Zack Hersh <zackshersh@gmail.com>
2021-10-01 21:04:14 -0400

    hey

--- public/listScripts.js
+++ public/listScripts.js
@@ -7,8 +7,9 @@ function buildList(){
 function getCompetitors(){
     fetch('https://tour-de-hill.herokuapp.com/api/competitors')
         .then((res) => {
-            console.log(res.json());
-            return res.json();
+            let data = res.json()
+            console.log(data);
+            return data;
         })
 }
 

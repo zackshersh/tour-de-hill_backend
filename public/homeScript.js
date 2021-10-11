ac74ea1e2d0d274ee9742473e07d0e38aba331ba
Zack Hersh <zackshersh@gmail.com>
2021-10-10 23:10:51 -0400

    form stuff

--- public/homeScript.js
+++ public/homeScript.js
@@ -1,19 +1,70 @@
 
 
-function init(){
-    canvInit()
+let ownBike = true;
+
+
+
+const choiceClick = (e) => {
+    let target = e.target;
+    let classes = e.target.classList;
+
+    let chosen = false;
+
+    classes.forEach(elemClass => {
+        if(elemClass == "chosen"){
+            chosen = true;
+        }
+    })
+
+    if(chosen){
+        return
+    } else {
+        e.target.classList.add('chosen');
+
+        if(e.target.textContent == 'Tar Heel Bike'){
+            ownBike = false
+
+            document.querySelector('#own').classList.remove('chosen')
+        } else {
+            ownBike = true
+
+            document.querySelector('#heel').classList.remove('chosen')
+        }
+    }
+
+    console.log(ownBike)
+
+
 }
 
-let c;
-let ctx;
+document.querySelectorAll('.choice').forEach(choice => choice.addEventListener('mousedown', choiceClick))
 
-function canvInit(){
-    c = document.querySelector('canvas');
-    c.width = window.innerWidth;
-    c.height = window.innerHeight;
-    ctx = c.getContext('2d');
+
+
+
+
+const submit = () => {
+
+    let data = {
+        first_name: document.querySelector('#first-name').textContent,
+        last_name: document.querySelector('#last-name').textContent,
+        own_bike: ownBike
+    }
+
+    fetch('https://tour-de-hill.herokuapp.com/api/competitor', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
 }
 
 
 
-init()
+document.querySelector('#register-button').addEventListener('mousedown',submit)
